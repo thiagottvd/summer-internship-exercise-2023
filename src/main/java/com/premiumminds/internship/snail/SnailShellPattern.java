76805b5bd7dda8517f7c197553e12d74944a45b2
fc53636 <fc53636@alunos.ciencias.ulisboa.pt>
2023-06-07 17:37:43 +0100

    Implemented getSnailShell method and its auxiliary method; changed the source compatibility level to Java 1.8.

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,5 +1,7 @@
 package com.premiumminds.internship.snail;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
@@ -14,6 +16,45 @@ class SnailShellPattern implements ISnailShellPattern {
    * @return order array of values thar represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    int matrixLength = matrix[0].length;
+    return executorService.submit( ()-> (matrixLength == 0) ? new int[0] : getSnailShellPattern(matrix, matrixLength));
+  }
+
+  /**
+   * Method to get snail-shell pattern for a given NxN matrix.
+   *
+   * @param matrix matrix of numbers to go through.
+   * @param matrixLength the matrix length.
+   * @return order array of values thar represent a snail shell pattern.
+   */
+  private int[] getSnailShellPattern(int[][] matrix, int matrixLength) {
+    int[] snailShellPattern = new int[matrixLength * matrixLength];
+    int leftBound = 0;
+    int rightBound = matrixLength - 1;
+    int topBound = 0;
+    int bottomBound = matrixLength - 1;
+    int arrayIndex = 0;
+
+    while (leftBound <= rightBound && topBound <= bottomBound) {
+      for (int j = leftBound; j <= rightBound; j++) {
+        snailShellPattern[arrayIndex++] = matrix[topBound][j];
+      }
+      topBound++;
+      for (int i = topBound; i <= bottomBound; i++) {
+        snailShellPattern[arrayIndex++] = matrix[i][rightBound];
+      }
+      rightBound--;
+      for (int j = rightBound; j >= leftBound; j--) {
+        snailShellPattern[arrayIndex++] = matrix[bottomBound][j];
+      }
+      bottomBound--;
+      for (int i = bottomBound; i >= topBound; i--) {
+        snailShellPattern[arrayIndex++] = matrix[i][leftBound];
+      }
+      leftBound++;
+    }
+    return snailShellPattern;
+  }
+
 }
